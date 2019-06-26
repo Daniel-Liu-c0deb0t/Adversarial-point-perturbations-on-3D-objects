045f2267b0926fcd0ab336c81c6f895f7e37315e
c0deb0t <daniel.liu02@gmail.com>
2019-06-25 21:45:02 -0700

    Added more attacks

--- src/adversarial_attacks.py
+++ src/adversarial_attacks.py
@@ -20,6 +20,26 @@ def iter_l2_attack_n_proj(model, x, y, params):
 
     return x_perturb
 
+def mom_l2_attack_n_proj(model, x, y, params):
+    epsilon = params["epsilon"]
+    mu = params["mu"]
+    n = params["n"]
+    tau = params["tau"]
+
+    epsilon = epsilon / float(n)
+    tree = PerturbProjTree(x, thickness = tau)
+    x_perturb = x
+    grad = np.zeros(x.shape)
+
+    for i in range(n):
+        curr_grad = model.grad_fn(x_perturb, y)
+        grad = mu * grad + curr_grad / np.sqrt(np.sum(curr_grad ** 2))
+        perturb = epsilon * grad / np.sqrt(np.sum(grad ** 2))
+        x_perturb = x_perturb + perturb
+        x_perturb = tree.project(x_perturb, perturb)
+
+    return x_perturb
+
 def iter_l2_attack_1_proj(model, x, y, params):
     epsilon = params["epsilon"]
     n = params["n"]
@@ -34,6 +54,32 @@ def iter_l2_attack_1_proj(model, x, y, params):
         perturb = epsilon * grad / np.sqrt(np.sum(grad ** 2))
         x_perturb = x_perturb + perturb
 
+    x_perturb = tree.project(x_perturb, perturb)
+
+    return x_perturb
+
+def iter_l2_attack(model, x, y, params):
+    epsilon = params["epsilon"]
+    n = params["n"]
+
+    epsilon = epsilon / float(n)
+    x_perturb = x
+
+    for i in range(n):
+        grad = model.grad_fn(x_perturb, y)
+        perturb = epsilon * grad / np.sqrt(np.sum(grad ** 2))
+        x_perturb = x_perturb + perturb
+
+    return x_perturb
+
+def normal_jitter(model, x, y, params):
+    epsilon = params["epsilon"]
+    tau = params["tau"]
+
+    tree = PerturbProjTree(x, thickness = tau)
+    perturb = np.random.normal(size = x.shape)
+    perturb = epsilon * perturb / np.sqrt(np.sum(perturb ** 2))
+    x_perturb = x + perturb
     x_perturb = tree.project(x_perturb, perturb)
 
     return x_perturb
--- src/run_adversarial_attacks.py
+++ src/run_adversarial_attacks.py
@@ -18,7 +18,10 @@ test_models = (0,)
 
 attacks = (
         ("iter_l2_attack_1_proj", adversarial_attacks.iter_l2_attack_1_proj, {"epsilon": 1.0, "n": 10, "tau": 0.05}),
+        ("iter_l2_attack", adversarial_attacks.iter_l2_attack, {"epsilon": 1.0, "n": 10}),
+        ("normal_jitter", adversarial_attacks.normal_jitter, {"epsilon": 1.0, "tau": 0.05}),
         ("iter_l2_attack_n_proj", adversarial_attacks.iter_l2_attack_n_proj, {"epsilon": 1.0, "n": 10, "tau": 0.05}),
+        ("mom_l2_attack_n_proj", adversarial_attacks.mom_l2_attack_n_proj, {"epsilon": 1.0, "mu": 1.0, "n": 10, "tau": 0.05}),
         ("iter_l2_attack_1_sampling", adversarial_attacks.iter_l2_attack_1_sampling, {"epsilon": 1.0, "n": 10, "k": 400, "kappa": 10, "tri_all_points": True})
 )
 
