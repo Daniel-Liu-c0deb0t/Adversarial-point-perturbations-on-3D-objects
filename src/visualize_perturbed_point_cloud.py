050b25919d3a8781d16fe4a976883dba5eaa20e9
c0deb0t <daniel.liu02@gmail.com>
2019-07-29 11:35:56 -0700

    Modified visualization code

--- src/visualize_perturbed_point_cloud.py
+++ src/visualize_perturbed_point_cloud.py
@@ -4,11 +4,12 @@ from mpl_toolkits.mplot3d import Axes3D
 
 np.random.seed(1234)
 
-view_label = "airplane"
+view_label = "stool"
 offset_idx = 0
-f = np.load("../output_save/final/1564384193_pointnet_iter_l2_attack_sinks_none.npz")
+f = np.load("../output_save/final/1564208936_pointnet_iter_l2_adversarial_sticks_none.npz")
 shape_names = [line.rstrip() for line in open("/media/sf_Xubuntu_shared/modelnet40_pc/shape_names.txt")]
 show_grads = True
+show_true = False
 
 x = f["x"]
 y_pred = f["y_pred"]
@@ -22,7 +23,7 @@ print("Shape:", x.shape)
 print("Labels:", [shape_names[idx] for idx in np.unique(y_pred_idx)])
 print("Successful attacks:", np.count_nonzero(y_pred_idx != y_adv_pred_idx))
 
-avg_zero_grad = np.sum(np.all(np.isclose(x_adv - x, 0.0), axis = 2)) / float(len(x))
+avg_zero_grad = np.sum(np.all(np.isclose(grad_adv, 0.0), axis = 2)) / float(len(x))
 print("Average number of points with zero gradients:", avg_zero_grad)
 
 print("Selected label:", view_label)
@@ -39,32 +40,38 @@ print("Clean prediction confidence:", y_pred[match_idx[offset_idx]][y_pred_idx_v
 print("Adversarial prediction confidence:", y_adv_pred[match_idx[offset_idx]][y_adv_pred_idx_view])
 print("Shape index:", match_idx[offset_idx])
 
-plt.figure(figsize = (30, 15))
-
 def scale_plot():
-    plt.gca().auto_scale_xyz((-1, 1), (-1, 1), (-1, 1))
-    plt.gca().view_init(0, 0)
+    scale = 0.7
+    plt.gca().auto_scale_xyz((-scale, scale), (-scale, scale), (-scale, scale))
+    plt.gca().view_init(-30, 200)
     plt.axis("off")
 
-plt.subplot(121, projection = "3d")
+if show_true:
+    plt.figure(figsize = (30, 15))
 
-plt.gca().scatter(*x_view.T, zdir = "y", s = 20, c = x_view.T[1], cmap = "winter")
+    plt.subplot(121, projection = "3d")
 
-scale_plot()
+    plt.gca().scatter(*x_view.T, zdir = "y", s = 50, c = x_view.T[1], cmap = "winter")
 
-plt.subplot(122, projection = "3d")
+    scale_plot()
+
+    plt.subplot(122, projection = "3d")
+else:
+    plt.figure(figsize = (15, 15))
+    plt.subplot(111, projection = "3d")
 
 if show_grads:
     grad_adv_view = np.linalg.norm(grad_adv_view, axis = 1)
     close_to_zero = np.isclose(grad_adv_view, 0.0)
     point_color = np.logical_not(close_to_zero).astype(float)
 
-    plt.gca().scatter(*x_adv_view.T, zdir = "y", s = 20, c = point_color, cmap = "winter")
+    plt.gca().scatter(*x_adv_view.T, zdir = "y", s = 50, c = -point_color, cmap = "winter")
 else:
-    plt.gca().scatter(*x_adv_view.T, zdir = "y", s = 20, c = x_adv_view.T[1], cmap = "winter")
+    plt.gca().scatter(*x_adv_view.T, zdir = "y", s = 50, c = x_adv_view.T[1], cmap = "winter")
 
 scale_plot()
 
 plt.subplots_adjust(left = 0, bottom = 0, right = 1, top = 1, wspace = 0, hspace = 0)
 plt.tight_layout()
+plt.savefig("../figures/adv_sticks_grads.pdf", bbox_inches = "tight")
 plt.show()
