210e1e7693bddd8a20b85b962d92d3b26657be4d
Daniel Liu <daniel.liu02@gmail.com>
2020-02-18 16:26:09 -0800

    Update run_old_adversarial_attacks.py

--- src/run_old_adversarial_attacks.py
+++ src/run_old_adversarial_attacks.py
@@ -22,25 +22,25 @@ test_models = (0,)
 attacks = (
         ("none", lambda _a, x, _b, _c: x, {}),
         ("iter_l2_attack_1_proj", adversarial_attacks.iter_l2_attack_1_proj, {"epsilon": 1.0, "n": 10, "tau": 0.05}),
-        ("iter_l2_attack", adversarial_attacks.iter_l2_attack, {"epsilon": 3.0, "n": 10}),
+        ("iter_l2_attack", adversarial_attacks.iter_l2_attack, {"epsilon": 2.0, "n": 20}),
         ("mom_l2_attack", adversarial_attacks.mom_l2_attack, {"epsilon": 1.0, "mu": 1.0, "n": 10}),
         ("normal_jitter", adversarial_attacks.normal_jitter, {"epsilon": 1.0, "tau": 0.05}),
-        ("iter_l2_attack_n_proj", adversarial_attacks.iter_l2_attack_n_proj, {"epsilon": 1.0, "n": 10, "tau": 0.05}),
+        ("iter_l2_attack_n_proj", adversarial_attacks.iter_l2_attack_n_proj, {"epsilon": 1.0, "n": 20, "tau": 0.05}),
         ("mom_l2_attack_n_proj", adversarial_attacks.mom_l2_attack_n_proj, {"epsilon": 1.0, "mu": 1.0, "n": 10, "tau": 0.05}),
         ("iter_l2_attack_1_sampling", adversarial_attacks.iter_l2_attack_1_sampling, {"epsilon": 3.0, "n": 10, "k": 500, "kappa": 10, "tri_all_points": True}),
         ("iter_l2_attack_1_sampling_all", adversarial_attacks.iter_l2_attack_1_sampling_all, {"epsilon": 3.0, "n": 10, "k": 500, "kappa": 10, "tri_all_points": True}),
-        ("iter_l2_attack_n_sampling", adversarial_attacks.iter_l2_attack_n_sampling, {"epsilon": 3.0, "n": 10, "k": 500, "kappa": 10, "tri_all_points": True}),
+        ("iter_l2_attack_n_sampling", adversarial_attacks.iter_l2_attack_n_sampling, {"epsilon": 2.0, "n": 20, "k": 500, "kappa": 10, "tri_all_points": True}),
         ("iter_l2_attack_1_sampling_rbf", adversarial_attacks.iter_l2_attack_1_sampling_rbf, {"epsilon": 3.0, "n": 10, "k": 500, "kappa": 10, "num_farthest": None, "shape": 5.0}),
         ("iter_l2_attack_n_sampling_rbf", adversarial_attacks.iter_l2_attack_n_sampling_rbf, {"epsilon": 3.0, "n": 10, "k": 500, "kappa": 10, "num_farthest": None, "shape": 5.0}),
         ("iter_l2_attack_top_k", adversarial_attacks.iter_l2_attack_top_k, {"epsilon": 3.0, "n": 10, "top_k": 10}),
-        ("iter_l2_adversarial_sticks", adversarial_attacks.iter_l2_adversarial_sticks, {"epsilon": 3.0, "n": 10, "top_k": 10, "sigma": 200}),
+        ("iter_l2_adversarial_sticks", adversarial_attacks.iter_l2_adversarial_sticks, {"epsilon": 2.0, "n": 20, "top_k": 20, "sigma": 200}),
         ("iter_l2_attack_fft", adversarial_attacks.iter_l2_attack_fft, {"epsilon": 20.0, "n": 10}),
-        ("iter_l2_attack_sinks", adversarial_attacks.iter_l2_attack_sinks, {"eta": 0.1, "mu": 5.0, "lambda_": 100000.0, "n": 20, "num_sinks": 100}),
-        ("chamfer_attack", adversarial_attacks.chamfer_attack, {"eta": 0.1, "alpha": 100000.0, "lambda_": 0.01, "n": 20})
+        ("iter_l2_attack_sinks", adversarial_attacks.iter_l2_attack_sinks, {"eta": 0.01, "mu": 5.0, "lambda_": 100000.0, "n": 20, "num_sinks": 30}),
+        ("chamfer_attack", adversarial_attacks.chamfer_attack, {"eta": 0.01, "alpha": 1000.0, "lambda_": 0.003, "n": 20})
 )
 
 fft = False
-sink = 100
+sink = 30
 chamfer = True
 
 test_attacks = (0, 2, 5, 9, 13, 15, 16)
